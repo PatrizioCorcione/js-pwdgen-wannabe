207cad6eb83aebeb638a3ea545e5dd4f528d2c8f
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-02-19 15:42:13 +0100

    aggiunto style color

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const nome = prompt('Scrivi il tuo nome');
 
 const cognome = prompt('Scrivi il tuo cognome');
 
-const colore = prompt('Scrivi il tuo colore preferito');
+const colore = prompt('Scrivi il tuo colore preferito in inglese');
 
 const numero = prompt('Scrivi un numero per rendere piu sicura la password');
 
@@ -12,6 +12,6 @@ console.log(password);
 
 document.getElementById("txt-password").innerHTML =`
 Ciao ${nome},<br>
-Questa e la tua nuova password : <strong class="${"color"}">${password}</strong> 
+Questa e la tua nuova password : <strong style="color:${colore}">${password}</strong> 
 
 `
