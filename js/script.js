44abf4491bd14acc189259d7d2609343b08798a6
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-02-19 13:32:49 +0100

    stampare in console

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const nome = prompt('Scrivi il tuo nome');
+
+const cognome = prompt('Scrivi il tuo cognome');
+
+const colore = prompt('Scrivi il tuo colore preferito');
+
+const numero = prompt('Scrivi un numero per rendere piu sicura la password');
+
+const password = nome + cognome + colore + numero;
+
+console.log(password);
